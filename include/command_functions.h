e96edaf63a95a163324d2463c67f948fc0db9c7e
samuko-things <samuel.c.agba@gmail.com>
2026-02-17 18:35:26 +0100

    update default filter gain

--- include/command_functions.h
+++ include/command_functions.h
@@ -96,7 +96,7 @@ int status;
 
 MadgwickFilter madgwickFilter;
 
-float filterGain = 0.5;
+float filterGain = 0.25;
 int worldFrameId = 1; // 0 - NWU,  1 - ENU,  2 - NED
 
 // initial i2cAddress
@@ -227,7 +227,7 @@ void resetParamsInStorage(){
     storage.putFloat(magAmatR1_key[i], 0.0);
     storage.putFloat(magAmatR2_key[i], 0.0);
   }
-  storage.putFloat(filterGain_key, 0.5);
+  storage.putFloat(filterGain_key, 0.25);
   storage.putFloat(cutOffFreq_key, 1.0);
   storage.putInt(worldFrameId_key, 1);
   storage.putUChar(i2cAddress_key, 0x68);
@@ -267,7 +267,7 @@ void loadStoredParams(){
     magAmat[1][i] = storage.getFloat(magAmatR1_key[i], 0.0);
     magAmat[2][i] = storage.getFloat(magAmatR2_key[i], 0.0);
   }
-  filterGain = storage.getFloat(filterGain_key, 0.5);
+  filterGain = storage.getFloat(filterGain_key, 0.25);
   cutOffFreq = storage.getFloat(cutOffFreq_key, 1.0);
   worldFrameId = storage.getInt(worldFrameId_key, 1);
   i2cAddress = storage.getUChar(i2cAddress_key, 0x68);
